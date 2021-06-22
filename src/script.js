4acfb532bf2db4c8a86e8888ee67ea5e97530409
Atichka <nosik_atichka@mail.ru>
2021-06-22 17:37:12 +0300

    Переменная исправлена на const

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const forms = [...document.forms];
 forms.forEach((form) => {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        let formData = new FormData(form);
+        const formData = new FormData(form);
         let obj = {};
         for (let [name, value] of formData) {
             obj[name] = value;
