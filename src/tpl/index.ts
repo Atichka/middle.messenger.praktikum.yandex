bf89d609e89077b4db36e7eb6bbb0070946f8765
Atichka <nosik_atichka@mail.ru>
2021-08-30 17:47:57 +0300

    Рендеринг страницы ошибки 500

--- src/tpl/index.ts
+++ src/tpl/index.ts
@@ -4,6 +4,7 @@ import chatsTemplate from './pages/chatPage.pug';
 import profileTemplate from './pages/profile.pug';
 import passwordEditTemplate from './pages/passwordEdit.pug';
 import error404Template from './pages/error404.pug';
+import error500Template from './pages/error500.pug';
 
 const TEMPLATES = {
     login: loginTemplate,
@@ -11,7 +12,8 @@ const TEMPLATES = {
     chats: chatsTemplate,
     profile: profileTemplate,
     passwordEdit: passwordEditTemplate,
-    error404: error404Template
+    error404: error404Template,
+    error500: error500Template
 };
 
 export function renderTemplate(name: any, locals: object = {}, parent: HTMLElement = document.body) {
