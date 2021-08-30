9fee737d73f234ad038e237c7af6b4014b7a405a
Atichka <nosik_atichka@mail.ru>
2021-08-30 13:53:18 +0300

    Удаление отладки

--- src/tpl/pages/login.ts
+++ src/tpl/pages/login.ts
@@ -3,6 +3,5 @@ import {renderTemplate} from '../index'
 const loginLink = document.querySelector('.login');
 
 loginLink.addEventListener('click', () => {
-    console.log("!")
     renderTemplate('login')
 });
