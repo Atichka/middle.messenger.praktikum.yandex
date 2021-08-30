fd51b333844d5fae80a1d1722b07a94602877ba7
Atichka <nosik_atichka@mail.ru>
2021-08-30 17:44:35 +0300

    Рендеринг страницы ошибки 404

--- src/tpl/index.ts
+++ src/tpl/index.ts
@@ -3,13 +3,15 @@ import signinTemplate from './pages/signin.pug';
 import chatsTemplate from './pages/chatPage.pug';
 import profileTemplate from './pages/profile.pug';
 import passwordEditTemplate from './pages/passwordEdit.pug';
+import error404Template from './pages/error404.pug';
 
 const TEMPLATES = {
     login: loginTemplate,
     signin: signinTemplate,
     chats: chatsTemplate,
     profile: profileTemplate,
-    passwordEdit: passwordEditTemplate
+    passwordEdit: passwordEditTemplate,
+    error404: error404Template
 };
 
 export function renderTemplate(name: any, locals: object = {}, parent: HTMLElement = document.body) {
