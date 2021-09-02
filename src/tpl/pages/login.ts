796e9c1ca2d0c2d51ec48a1e9349bb334a4818f0
Atichka <nosik_atichka@mail.ru>
2021-09-02 16:54:50 +0300

    Правка вывода данных для страницы login

--- src/tpl/pages/login.ts
+++ src/tpl/pages/login.ts
@@ -5,3 +5,15 @@ const loginLink = document.querySelector('.loginLink');
 loginLink.addEventListener('click', () => {
     renderTemplate('login')
 });
+
+function sendData() {
+    const form = document.querySelector('.form');
+    const formData: any = new FormData(form);
+    let obj: Record<string, unknown> = {};
+    for (let [name, value] of formData) {
+        obj[name] = value;
+    }
+    console.log(obj);
+}
+
+window.sendData = sendData
