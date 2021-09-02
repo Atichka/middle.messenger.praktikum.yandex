29d392efb23c50709c119707fd5239e657ce3511
Atichka <nosik_atichka@mail.ru>
2021-09-02 16:56:58 +0300

    Правка вывода данных в консоль для страницы signin

--- src/tpl/pages/signin.ts
+++ src/tpl/pages/signin.ts
@@ -5,3 +5,15 @@ const signinLink = document.querySelector('.signinLink');
 signinLink.addEventListener('click', () => {
     renderTemplate('signin')
 });
+
+function sendData() {
+    const form = document.querySelector('.form');
+    const formData: any = new FormData(form);
+    let obj: Record<string, unknown> = {};
+    for (let [name, value] of formData) {
+        obj[name] = value;
+    }
+    console.log(obj);
+}
+
+window.sendData = sendData
