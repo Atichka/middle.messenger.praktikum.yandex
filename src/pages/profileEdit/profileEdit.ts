5d9b7740ad85a96448ffcaf279cdfe40cd9f1ead
Atichka <nosik_atichka@mail.ru>
2021-09-19 02:22:44 +0300

    Добавлен шаблон формы

--- src/pages/profileEdit/profileEdit.ts
+++ src/pages/profileEdit/profileEdit.ts
@@ -3,6 +3,7 @@ import template from "./profileEdit.pug";
 import {Button} from "../../components/Button/button";
 import {compile} from "../../util/compile";
 import {Input} from "../../components/Input/input";
+import {FormProfileEdit} from "../../components/FormProfileEdit/formProfileEdit";
 
 export class ProfileEditPage extends Block {
     constructor() {
@@ -10,116 +11,116 @@ export class ProfileEditPage extends Block {
     }
 
     public render(): DocumentFragment {
-        const buttonSave = new Button( {
-            text: "Сохранить",
-            events: {
-                click: () => console.log("buttonSave")
-            },
-            classNames: ["profile__button"]
-        });
-        const buttonBack = new Button( {
-            text: "←",
-            events: {
-                click: () => console.log("buttonBack")
-            },
-            classNames: ["profile__button-back"]
-        });
-        const buttonChange = new Button( {
-            text: "Поменять",
-            events: {
-                click: () => console.log("buttonChange")
-            },
-            classNames: ["profile__button"]
-        });
-        const inputEmail = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "userEmail",
-            type: "email",
-            name: "email",
-            minlength: 2,
-            maxlength: 30,
-            required: "",
-            placeholder: "pochta@yandex.ru",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputLogin = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "userLogin",
-            type: "text",
-            name: "login",
-            minlength: 2,
-            maxlength: 30,
-            required: "",
-            placeholder: "ivanivanov",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputFirstName = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "newFirstName",
-            type: "text",
-            name: "first_name",
-            minlength: 2,
-            maxlength: 30,
-            required: "",
-            placeholder: "Иван",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputSecondName = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "newSecondName",
-            type: "text",
-            name: "second_name",
-            minlength: 2,
-            maxlength: 30,
-            required: "",
-            placeholder: "Иванов",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputDisplayName = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "newDisplayName",
-            type: "text",
-            name: "display_name",
-            minlength: 2,
-            maxlength: 30,
-            required: "",
-            placeholder: "Иван",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputPhone = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "tel",
-            type: "phone",
-            name: "display_name",
-            minlength: 11,
-            maxlength: 18,
-            required: "",
-            placeholder: "+79099673030",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
+        const formProfileEdit = new FormProfileEdit( {
+            classNames: ["form"],
+            id: "formData",
+            name: "formData",
+            buttonSave: new Button( {
+                text: "Сохранить",
+                events: {
+                    click: (e) => this.sendData(e),
+                },
+                classNames: ["profile__button"]
+            }),
+            buttonBack: new Button( {
+                text: "←",
+                events: {
+                    click: () => console.log("buttonBack")
+                },
+                classNames: ["profile__button-back"]
+            }),
+            buttonChange: new Button( {
+                text: "Поменять",
+                events: {
+                    click: (e) => this.sendData(e),
+                },
+                classNames: ["profile__button"]
+            }),
+            inputEmail: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "userEmail",
+                type: "email",
+                name: "email",
+                minlength: 2,
+                maxlength: 30,
+                required: "",
+                placeholder: "pochta@yandex.ru",
+            }),
+            inputLogin: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "userLogin",
+                type: "text",
+                name: "login",
+                minlength: 2,
+                maxlength: 30,
+                required: "",
+                placeholder: "ivanivanov",
+            }),
+            inputFirstName: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "newFirstName",
+                type: "text",
+                name: "first_name",
+                minlength: 2,
+                maxlength: 30,
+                required: "",
+                placeholder: "Иван",
+            }),
+            inputSecondName: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "newSecondName",
+                type: "text",
+                name: "second_name",
+                minlength: 2,
+                maxlength: 30,
+                required: "",
+                placeholder: "Иванов",
+            }),
+            inputDisplayName: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "newDisplayName",
+                type: "text",
+                name: "display_name",
+                minlength: 2,
+                maxlength: 30,
+                required: "",
+                placeholder: "Иван",
+            }),
+            inputPhone: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "tel",
+                type: "phone",
+                name: "display_name",
+                minlength: 11,
+                maxlength: 18,
+                required: "",
+                placeholder: "+79099673030",
+            })
+        })
         return compile(template,{
-            buttonSave: buttonSave,
-            buttonBack: buttonBack,
-            buttonChange: buttonChange,
-            inputEmail: inputEmail,
-            inputLogin: inputLogin,
-            inputFirstName: inputFirstName,
-            inputSecondName: inputSecondName,
-            inputDisplayName: inputDisplayName,
-            inputPhone: inputPhone,
+            formProfileEdit: formProfileEdit,
         });
+    }
+
+    sendData(e) {
+        e.preventDefault();
+        const userEmail = document.querySelector('#userEmail');
+        console.log('email', userEmail.value);
+
+        const userLogin = document.querySelector('#userLogin');
+        console.log('login', userLogin.value);
+
+        const firstName = document.querySelector('#newFirstName');
+        console.log('firstName', firstName.value);
+
+        const secondName = document.querySelector('#newSecondName');
+        console.log('firstName', secondName.value);
+
+        const displayName = document.querySelector('#newDisplayName');
+        console.log('displayName', displayName.value);
+
+        const phone = document.querySelector('#tel');
+        console.log('phone', phone.value);
     }
 }
 
