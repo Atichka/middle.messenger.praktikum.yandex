c29152c2cffffab2ff407ae83e5987b9353d12dd
Atichka <nosik_atichka@mail.ru>
2021-09-19 01:13:16 +0300

    Вынесена форма в компонент, добавлена проверка паролей

---
+++ src/components/FormPasswordEdit/formPasswordEdit.ts
@@ -0,0 +1,23 @@
+import {Block} from "../../util/block";
+import template from "./template.pug";
+import {compile} from "../../util/compile";
+
+export class FormPasswordEdit extends Block {
+    constructor(props) {
+        // Создаём враппер дом-элемент button
+        super("form", props);
+    }
+
+    public render(): DocumentFragment {
+        if(Array.isArray(this.props.classNames)) {
+            this._element.classList.add(...this.props.classNames)
+        } else {
+            this._element.classList.add(this.props.classNames)
+        }
+        this._element.setAttribute('id', this.props.id);
+        this._element.setAttribute('name', this.props.name);
+        return compile(template,
+            { ...this.props }
+        );
+    }
+}
--- src/pages/passwordEdit/passwordEdit.ts
+++ src/pages/passwordEdit/passwordEdit.ts
@@ -3,6 +3,7 @@ import template from "./passwordEdit.pug";
 import {Button} from "../../components/Button/button";
 import {compile} from "../../util/compile";
 import {Input} from "../../components/Input/input";
+import {FormPasswordEdit} from "../../components/FormPasswordEdit/formPasswordEdit";
 
 export class PasswordEditPage extends Block {
     constructor() {
@@ -10,60 +11,69 @@ export class PasswordEditPage extends Block {
     }
 
     public render(): DocumentFragment {
-        const buttonSave = new Button( {
-            text: "Сохранить",
-            events: {
-                click: () => console.log("buttonSave")
-            },
-            classNames: ["profile__button"]
-        });
-        const buttonBack = new Button( {
-            text: "←",
-            events: {
-                click: () => console.log("buttonBack")
-            },
-            classNames: ["profile__button-back"]
-        });
-        const inputOldPassword = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "oldPassword",
-            type: "password",
-            name: "password",
-            required: "",
-            placeholder: "Пароль",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputNewPassword = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "newPassword",
-            type: "password",
-            name: "newPassword",
-            required: "",
-            placeholder: "Пароль",
-            events: {
-                focus: () => onFocus(this),
-            },
-        });
-        const inputNewPasswordRepeat = new Input( {
-            classNames: ["profile__input", "text__grey", "profile__text"],
-            id: "newPasswordRepeat",
-            type: "password",
-            name: "newPasswordRepeat",
-            required: "",
-            placeholder: "Пароль",
-            events: {
-                focus: () => onFocus(this),
-            },
+        const formPasswordEdit = new FormPasswordEdit( {
+            classNames: ["form"],
+            id: "formPassword",
+            name: "formPassword",
+            buttonSave: new Button( {
+                text: "Сохранить",
+                events: {
+                    click: (e) => this.sendData(e),
+                },
+                classNames: ["profile__button"]
+            }),
+            buttonBack: new Button( {
+                text: "←",
+                events: {
+                    click: () => console.log("buttonBack")
+                },
+                classNames: ["profile__button-back"]
+            }),
+            inputOldPassword: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "oldPassword",
+                type: "password",
+                name: "password",
+                required: "",
+                placeholder: "Пароль",
+            }),
+            inputNewPassword: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "newPassword",
+                type: "password",
+                name: "newPassword",
+                required: "",
+                placeholder: "Пароль",
+            }),
+            inputNewPasswordRepeat: new Input( {
+                classNames: ["profile__input", "text__grey", "profile__text"],
+                id: "newPasswordRepeat",
+                type: "password",
+                name: "newPasswordRepeat",
+                required: "",
+                placeholder: "Пароль",
+            }),
         });
         return compile(template,{
-            buttonSave: buttonSave,
-            buttonBack: buttonBack,
-            inputOldPassword: inputOldPassword,
-            inputNewPassword: inputNewPassword,
-            inputNewPasswordRepeat: inputNewPasswordRepeat,
+            formPasswordEdit: formPasswordEdit,
         });
+    }
+
+    sendData(e) {
+        e.preventDefault();
+        const inputNewPassword = document.querySelector('#newPassword');
+        const inputNewPasswordRepeat = document.querySelector('#newPasswordRepeat');
+        const inputOldPassword = document.querySelector('#oldPassword');
+        const name = inputNewPasswordRepeat.name;
+        const spanNewPasswordRepeat = this._element.querySelector(`#error-${name}`);
+        if(inputNewPassword.value === inputNewPasswordRepeat.value) {
+            spanNewPasswordRepeat.classList.add("error-hide");
+        } else {
+            spanNewPasswordRepeat.classList.remove('error-hide');
+        }
+        console.log(inputOldPassword.value);
+        console.log(inputNewPassword.value);
+        console.log(inputNewPasswordRepeat.value);
     }
 }
 
