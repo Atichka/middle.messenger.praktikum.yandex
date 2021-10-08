b6780a347ae9345748fc6857ed069c99446857d5
Atichka <nosik_atichka@mail.ru>
2021-10-08 14:31:25 +0300

    удалена функция рендера страницы и подключен роутер

--- index.ts
+++ index.ts
@@ -1,21 +1,6 @@
-import {HomePage} from "./src/pages/home";
-import {Block} from "./src/util/block";
+import {Router} from './router'
 
-export function render(query: string, block: Block) {
-    const root = document.querySelector(query);
-
-    if(!root) {
-        throw new Error("Root not found");
-    }
-
-    root.innerHTML = '';
-
-    root.appendChild(block.getContent());
-
-    return root;
-}
-
-render('#app', new HomePage());
+console.log('router', Router);
 
 export function addClass(_element, props) {
     if(Array.isArray(props.classNames)) {
